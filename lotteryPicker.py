a55f86b1e20fe74d9811fa8509428a740cbc74cb
Brent Forrest <34299709+brentorama@users.noreply.github.com>
2017-12-06 17:29:34 +0900

    Lottery number picker
    A bit of python 100% guaranteed to pick winning lottery numbers.  Results may vary.

---
+++ lotteryPicker.py
@@ -0,0 +1,19 @@
+def lotteryPicker(numTix, picks, possible):
+    import random
+    import math
+    n = math.factorial(possible)
+    k = math.factorial(picks)* math.factorial(possible-picks)
+    combos = n/k
+    comboStr= '{:,.0f}'.format(combos) 
+    limit = 10000
+    tix = min(numTix, limit)
+    comTix = combos/tix
+    odds = '{:,.0f}'.format(comTix) 
+    print 'Calculating your winning numbers:'
+    for i in range(tix):
+        ints = sorted(random.sample(range(possible), picks))
+        print ints
+    
+    print '\nYou have a %i in %s chance of winning the jackpot \nOr 1 in %s\nor a %f%% chance' % (tix, comboStr, odds, 100* (float(1.0)/float(comTix))) 
+
+lotteryPicker(20, 6, 49)
